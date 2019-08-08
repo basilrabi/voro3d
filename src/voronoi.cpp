be6cb1b12d76a183bfc263f1fde14af9c31dfe71
basilrabi <ericbasil.rabi@gmail.com>
2019-08-08 09:43:07 +0800

    return NA if cell cant be computed

--- src/voronoi.cpp
+++ src/voronoi.cpp
@@ -109,70 +109,81 @@ Rcpp::StringVector voronoi( Rcpp::NumericVector x,
 
   // Compute voronoi cells
   voro::c_loop_order clo( con, po );
-  if ( clo.start() ) do if ( con.compute_cell( vc, clo ) ) {
-
-    // Store the coordinates of the particle
-    clo.pos( i, j, k );
-
-    // Store coordinates of each vertex. Each set of vertex coordinates is
-    // stored at every 3 elements in `vertices`
-    vc.vertices( i, j, k, vertices );
-
-    long unsigned int vCounter = 0;
-    for ( ; vCounter < vertices.size(); vCounter += 3 )
+  if ( clo.start() )
+  {
+    do
     {
-      point = std::to_string( vertices[vCounter] ) + " " +
-        std::to_string( vertices[vCounter + 1] ) + " " +
-        std::to_string( vertices[vCounter + 2] );
-      points.push_back( point );
-    }
-
-    polyhedralsurface = "POLYHEDRALSURFACE(";
-
-    // Append unique face data to polyhedral surface. Each face has 3 vertices
-    // and the data for each face indicates the indices of the 3 vertices. The
-    // control flow below is copied from voro++ since the API is hard to
-    // understand.
-    for ( ii = 1; ii < vc.p; ii++ )
-    {
-      for ( jj = 0; jj < vc.nu[ii]; jj++ )
+      if ( con.compute_cell( vc, clo ) )
       {
-        kk = vc.ed[ii][jj];
-        if ( kk >= 0 )
+
+        // Store the coordinates of the particle
+        clo.pos( i, j, k );
+
+        // Store coordinates of each vertex. Each set of vertex coordinates is
+        // stored at every 3 elements in `vertices`
+        vc.vertices( i, j, k, vertices );
+
+        long unsigned int vCounter = 0;
+        for ( ; vCounter < vertices.size(); vCounter += 3 )
         {
-          vc.ed[ii][jj] = -1 - kk;
-          ll = vc.cycle_up( vc.ed[ii][vc.nu[ii] + jj], kk );
-          mm = vc.ed[kk][ll];
-          vc.ed[kk][ll] = -1 - mm;
-          while ( mm != ii )
+          point = std::to_string( vertices[vCounter] ) + " " +
+            std::to_string( vertices[vCounter + 1] ) + " " +
+            std::to_string( vertices[vCounter + 2] );
+          points.push_back( point );
+        }
+
+        polyhedralsurface = "POLYHEDRALSURFACE(";
+
+        // Append unique face data to polyhedral surface. Each face has 3
+        // vertices and the data for each face indicates the indices of the 3
+        // vertices. The control flow below is copied from voro++ since the API
+        // is hard to understand.
+        for ( ii = 1; ii < vc.p; ii++ )
+        {
+          for ( jj = 0; jj < vc.nu[ii]; jj++ )
           {
-            nn = vc.cycle_up( vc.ed[kk][vc.nu[kk] + ll], mm );
-            polygon = "((" +
-              points[ii] + ", " +
-              points[kk] + ", " +
-              points[mm] + ", " +
-              points[ii] + "))";
+            kk = vc.ed[ii][jj];
+            if ( kk >= 0 )
+            {
+              vc.ed[ii][jj] = -1 - kk;
+              ll = vc.cycle_up( vc.ed[ii][vc.nu[ii] + jj], kk );
+              mm = vc.ed[kk][ll];
+              vc.ed[kk][ll] = -1 - mm;
+              while ( mm != ii )
+              {
+                nn = vc.cycle_up( vc.ed[kk][vc.nu[kk] + ll], mm );
+                polygon = "((" +
+                  points[ii] + ", " +
+                  points[kk] + ", " +
+                  points[mm] + ", " +
+                  points[ii] + "))";
 
-            if ( polyhedralsurface == "POLYHEDRALSURFACE(" )
-              polyhedralsurface += polygon;
-            else
-              polyhedralsurface += ", " + polygon;
+                if ( polyhedralsurface == "POLYHEDRALSURFACE(" )
+                  polyhedralsurface += polygon;
+                else
+                  polyhedralsurface += ", " + polygon;
 
-            kk = mm;
-            ll = nn;
-            mm = vc.ed[kk][ll];
-            vc.ed[kk][ll] = -1 - mm;
+                kk = mm;
+                ll = nn;
+                mm = vc.ed[kk][ll];
+                vc.ed[kk][ll] = -1 - mm;
+              }
+            }
           }
         }
+
+        polyhedralsurface += ")";
+        cellGeometry[cGCount] = polyhedralsurface;
+        points.clear();
       }
-    }
 
-    polyhedralsurface += ")";
-    cellGeometry[cGCount] = polyhedralsurface;
-    cGCount++;
-    points.clear();
+      else
+        cellGeometry[cGCount] = NA_STRING;
 
-  } while ( clo.inc() );
+      cGCount++;
+
+    } while ( clo.inc() );
+  }
 
   return cellGeometry;
 }
